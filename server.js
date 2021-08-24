4284994bafc8afef17a56538e7dc9b36c526fb79
yash03112000 <yashag@iitk.ac.in>
2021-08-25 00:34:51 +0530

    heroku

--- server.js
+++ server.js
@@ -23,12 +23,15 @@ app.prepare().then(() => {
 
 	const connectDB = async () => {
 		try {
-			const conn = await mongoose.connect(process.env.MONGO_URI, {
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-				useFindAndModify: false,
-				useCreateIndex: true,
-			});
+			const conn = await mongoose.connect(
+				dev ? process.env.MONGO_URI : process.env.MONGO_URI_PROD,
+				{
+					useNewUrlParser: true,
+					useUnifiedTopology: true,
+					useFindAndModify: false,
+					useCreateIndex: true,
+				}
+			);
 
 			mongoose.Promise = global.Promise;
 
